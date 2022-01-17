b549c5b8fc283c70c228d7172c0f01ca9a2aa562
Hisanobu Oda <watchmen0205@yahoo.co.jp>
2022-01-17 23:23:54 +0900

    learn minimum window substring

--- HardProblems/MinimumWindowSubstring.java
+++ HardProblems/MinimumWindowSubstring.java
@@ -22,6 +22,36 @@ class MinimumWindowSubstring {
         // tにあるユニークな文字数を格納
         int formed = 0;
 
-        
+        Map<Character, Integer> windowCounts = new HashMap<Character, Integer>();
+
+        int[] ans = { -1, 0, 0 };
+
+        while (r < s.length()) {
+            char c = s.charAt(r);
+            int count = windowCounts.getOrDefault(c, 0);
+            windowCounts.put(c, count + 1);
+            
+            if (dictT.containsKey(c) && windowCounts.get(c).intValue() == dictT.get(c).intValue()) {
+                formed++;
+            }
+
+            while(l <= r && formed == required) {
+                c = s.charAt(l);
+
+                if(ans[0] == -1 || r - l + 1 < ans[0]) {
+                    ans[0] = r - l + 1;
+                    ans[1] = l;
+                    ans[2] = r;
+                }
+
+                windowCounts.put(c, windowCounts.get(c) - 1);
+                if(dictT.containsKey(c) && windowCounts.get(c).intValue() < dictT.get(c).intValue()) {
+                    formed--;
+                }
+                l++;
+            }
+            r++;
+        }
+        return ans[0] == -1 ? "": s.substring(ans[1], ans[2] + 1);
     }
 }
