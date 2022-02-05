96cce5d6151eb44bc69bdbb004261eb1aa9ac00d
Hisanobu Oda <watchmen0205@yahoo.co.jp>
2022-02-05 23:30:11 +0900

    learn lowest common ancestor

--- MediumProblems/LowestCommonAncestor.java
+++ MediumProblems/LowestCommonAncestor.java
@@ -2,11 +2,38 @@ class TreeNode {
     int val;
     TreeNode left;
     TreeNode right;
-    TreeNode(int x) { val = x; }
+
+    TreeNode(int x) {
+        val = x;
+    }
 }
 
 class LowestCommonAncestor {
+    private TreeNode ans;
+
+    public boolean recurseTree(TreeNode currentNode, TreeNode p, TreeNode q) {
+
+        // If reached the end of a branch, return false
+        if (currentNode == null) {
+            return false;
+        }
+
+        int left = recurseTree(currentNode.left, p, q) ? 1 : 0;
+        int right = recurseTree(currentNode.right, p, q) ? 1 : 0;
+
+        int mid = (currentNode == p || currentNode == q) ? 1 : 0;
+
+        // If any two of the flags left, right or mid become true
+        if (mid + left + right >= 2) {
+            ans = currentNode;
+        }
+
+        return (mid + left + right > 0);
+    }
+
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        
+        ans = null;
+        recurseTree(root, p, q);
+        return ans;
     }
 }
