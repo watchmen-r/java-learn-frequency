f96e8d2bdf0b558d8117d71d9be1bc78ed797e97
Hisanobu Oda <watchmen0205@yahoo.co.jp>
2022-02-07 23:41:39 +0900

    learn letter combinations phone number

---
+++ MediumProblems/letterCombinationsPhoneNumber.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+class letterCombinationsPhoneNumber {
+    List<String> combinations = new ArrayList<>();
+    Map<Character, String> letters = Map.of('2', "abc", '3', "def", '4', "ghi", '5', "jkl", '6', "mno", '7', "pqrs",
+            '8', "tuv", '9', "wxyz");
+    
+    String phoneDigits;
+
+    public List<String> letterCombinations(String digits) {
+        // If the input is empty, immediately return an empty answer array
+        if(digits.length() == 0) {
+            return combinations;
+        }
+
+        phoneDigits = digits;
+
+        backtrack(0, new StringBuilder());
+        return combinations;
+    }
+
+    void backtrack(int index, StringBuilder path) {
+        // If the path is the same length as digits, we have a complete combination
+        if (path.length() == phoneDigits.length()) {
+            combinations.add(path.toString());
+            return;
+        }
+        
+        String possibleLetters = letters.get(phoneDigits.charAt(index));
+
+        for(char letter: possibleLetters.toCharArray()) {
+            path.append(letter);
+            backtrack(index + 1, path);
+            path.deleteCharAt(path.length() - 1);
+        }
+    }
+}
