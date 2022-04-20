17ff4363b6e73efb32cab84383e253dea410c1b2
Hisanobu Oda <watchmen0205@yahoo.co.jp>
2022-04-20 23:40:02 +0900

    learn top k frequent elements

---
+++ MediumProblems/TopKFrequentElements.java
@@ -0,0 +1,32 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        if (k == nums.length) {
+            return nums;
+        }
+
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int n : nums) {
+            map.put(n, map.getOrDefault(n, 0) + 1);
+        }
+
+        Queue<Integer> heap = new PriorityQueue<>((n1, n2) -> map.get(n1) - map.get(n2));
+
+        for (int n : map.keySet()) {
+            heap.add(n);
+            if (heap.size() > k) {
+                heap.poll();
+            }
+        }
+
+        int[] top = new int[k];
+        for (int i = 0; i < k; i++) {
+            top[i] = heap.poll();
+        }
+        return top;
+    }
+}
