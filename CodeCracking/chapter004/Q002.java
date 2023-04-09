08a971a5218b6e7a0b3dfb5b5d3d103bc39e00b9
watchmen-r <watchmen0205@yahoo.co.jp>
2023-04-09 15:24:58 +0900

    learn q001 of generate bst

---
+++ CodeCracking/chapter004/Q002.java
@@ -0,0 +1,30 @@
+package CodeCracking.chapter004;
+
+public class Q002 {
+    class Graph {
+        public Node[] nodes;
+    }
+
+    class Node {
+        public String name;
+        public Node nodeLeft;
+        public Node nodeRight;
+    }
+
+    public Node createBst(int[] array) {
+        generateBst(array, 0, array.length - 1);
+    }
+
+    private Node generateBst(int[] array, int left, int right) {
+        if (left > right) {
+            return null;
+        }
+
+        int half = (left + right) / 2;
+        Node node = new Node();
+
+        node.nodeLeft = generateBst(array, left, half - 1);
+        node.nodeRight = generateBst(array, half + 1, right);
+        return node;
+    }
+}
