b7786bab3b6e4627395ccb2867cf98749683b8b0
watchmen-r <watchmen0205@yahoo.co.jp>
2023-09-03 15:10:31 +0900

    learn two sum

---
+++ EasyProblems/TwoSum.java
@@ -0,0 +1,19 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] answer = new int[2];
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                answer[0] = map.get(target - nums[i]);
+                answer[1] = i;
+                return answer;
+            }
+            map.put(nums[i], i);
+        }
+        return null;
+    }
+}
