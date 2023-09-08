45053013b70baec917b61ce125c343839d98a1b0
watchmen-r <watchmen0205@yahoo.co.jp>
2023-09-08 18:33:23 +0900

    learn top k frequent elements again

--- MediumProblems/TopKFrequentElements.java
+++ MediumProblems/TopKFrequentElements.java
@@ -1,32 +1,32 @@
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.PriorityQueue;
-import java.util.Queue;
 
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        if (k == nums.length) {
-            return nums;
+        Map<Integer, Integer> countMap = new HashMap<>();
+        for (int num: nums) {
+            countMap.put(num, countMap.getOrDefault(num, 0) + 1);
         }
 
-        Map<Integer, Integer> map = new HashMap<>();
-        for (int n : nums) {
-            map.put(n, map.getOrDefault(n, 0) + 1);
+        // If write using the anonymous classes
+        // PriorityQueue<Integer> queue = new PriorityQueue<>(new Comparator<Integer>(){
+        //     public int compare(Integer a, Integer b){
+        //         return map.get(b)-map.get(a);
+        //     }
+        // });
+        PriorityQueue<Map.Entry<Integer, Integer>> queue = new PriorityQueue<>((a, b) -> (b.getValue() - a.getValue()));
+
+        for (Map.Entry<Integer, Integer> entry: countMap.entrySet()) {
+            queue.add(entry);
         }
 
-        Queue<Integer> heap = new PriorityQueue<>((n1, n2) -> map.get(n1) - map.get(n2));
-
-        for (int n : map.keySet()) {
-            heap.add(n);
-            if (heap.size() > k) {
-                heap.poll();
-            }
-        }
-
-        int[] top = new int[k];
+        int[] answer = new int[k];
         for (int i = 0; i < k; i++) {
-            top[i] = heap.poll();
+            answer[i] = queue.poll().getKey();
         }
-        return top;
+        return answer;
     }
 }
