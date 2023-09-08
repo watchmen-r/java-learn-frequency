09d68f3dd5c282e5cdbb9eaca7c26ea7ad2c8dcc
watchmen-r <watchmen0205@yahoo.co.jp>
2023-09-09 07:07:42 +0900

    learn top k frequent again

--- MediumProblems/TopKFrequentElements.java
+++ MediumProblems/TopKFrequentElements.java
@@ -3,6 +3,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.PriorityQueue;
+import java.util.Map.Entry;
 
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
@@ -27,6 +28,26 @@ class Solution {
         for (int i = 0; i < k; i++) {
             answer[i] = queue.poll().getKey();
         }
+        return answer;
+    }
+
+    public int[] topKFrequentAgain(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for(int num: nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        PriorityQueue<Map.Entry<Integer, Integer>> queue = new PriorityQueue<>((a, b) -> b.getValue() - a.getValue());
+        for (Entry<Integer, Integer> entry: map.entrySet()) {
+            queue.add(entry);
+        }
+
+        int[] answer = new int[k];
+        for (int i = 0; i < k; i++) {
+            answer[i] = queue.poll().getKey();
+        }
+
         return answer;
     }
 }
