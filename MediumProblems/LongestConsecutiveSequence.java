97b5d101b152960bf4311e12d2aaa8921c4def52
watchmen-r <watchmen0205@yahoo.co.jp>
2023-09-12 07:18:17 +0900

    learn longest consecutive sequence again

--- MediumProblems/LongestConsecutiveSequence.java
+++ MediumProblems/LongestConsecutiveSequence.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -25,6 +26,28 @@ class Solution {
                 set.remove(num);
             }
             max = Math.max(max, count);
+        }
+        return max;
+    }
+
+    public int longestConsecutiveAgain(int[] nums) {
+        int max = 0;
+        Set<Integer> set = IntStream.of(nums).boxed().collect(Collectors.toSet());
+
+        for (int i = 0; i < nums.length; i++) {
+            int consecutive = 1;
+            int target = nums[i];
+            while(set.contains(--target)) {
+                consecutive++;
+                set.remove(target);
+            }
+            target = nums[i];
+            while(set.contains(++target)) {
+                consecutive++;
+                set.remove(target);
+            }
+
+            max = Math.max(max, consecutive);
         }
         return max;
     }
