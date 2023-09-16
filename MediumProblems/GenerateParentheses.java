68ae736cf3019e40c01c8ac89a371e3584e0a0c0
watchmen-r <watchmen0205@yahoo.co.jp>
2023-09-16 14:45:53 +0900

    learn generate parentheses

--- MediumProblems/GenerateParentheses.java
+++ MediumProblems/GenerateParentheses.java
@@ -2,38 +2,31 @@ import java.util.ArrayList;
 import java.util.List;
 
 class Solution {
+    List<String> temp = new ArrayList<>();
     List<String> answer = new ArrayList<>();
-
-    void recursive(StringBuilder sb, int n, int leftCount, int rightCount) {
-        if(leftCount < rightCount) {
-            return;
-        }
-        if(n == leftCount && n == rightCount) {
-            answer.add(sb.toString());
-            return;
-        }
-
-        if(leftCount < n) {
-            sb.append("(");
-            leftCount++;
-            recursive(sb, n, leftCount, rightCount);
-            sb.deleteCharAt(leftCount + rightCount - 1);
-            leftCount--;
-        }
-
-        if(rightCount < n) {
-            sb.append(")");
-            rightCount++;
-            recursive(sb, n, leftCount, rightCount);
-            sb.deleteCharAt(leftCount + rightCount - 1);
-            rightCount--;
-        }
-    }
-    
     public List<String> generateParenthesis(int n) {
-        StringBuilder sb = new StringBuilder();
-        sb.append("(");
-        recursive(sb, n, 1, 0);
+        backtrack(0, 0, n);
         return answer;
+    }
+
+    void backtrack(int openN, int closeN, int n) {
+        if (temp.size() == n * 2) {
+            String tmpAnswer = temp.stream().reduce("", (sub, element) -> sub + element);
+            answer.add(tmpAnswer);
+        }
+
+        if (openN < n) {
+            temp.add("(");
+            backtrack(++openN, closeN, n);
+            temp.remove(temp.size()-1);
+            openN--;
+        }
+
+        if (closeN < n && closeN < openN) {
+            temp.add(")");
+            backtrack(openN, ++closeN, n);
+            temp.remove(temp.size()-1);
+            closeN--;
+        }
     }
 }
