26a0d7fae6025ab062ada1a21499a9cbfadaa9fd
watchmen-r <watchmen0205@yahoo.co.jp>
2023-09-18 16:36:11 +0900

    review longest consecutive

--- MediumProblems/LongestConsecutiveSequence.java
+++ MediumProblems/LongestConsecutiveSequence.java
@@ -5,43 +5,18 @@ import java.util.stream.IntStream;
 
 class Solution {
     public int longestConsecutive(int[] nums) {
+        Set<Integer> set = Arrays.stream(nums).boxed().collect(Collectors.toSet());
+
         int max = 0;
 
-        Set<Integer> set = IntStream.of(nums).boxed().collect(Collectors.toSet());
-
-        for (int i = 0; i < nums.length; i++) {
-            int count = 1;
-
-            // look left
-            int num = nums[i];
-            while (set.contains(--num)) {
-                count++;
-                set.remove(num);
-            }
-
-            // look right
-            num = nums[i];
-            while(set.contains(++num)) {
-                count++;
-                set.remove(num);
-            }
-            max = Math.max(max, count);
-        }
-        return max;
-    }
-
-    public int longestConsecutiveAgain(int[] nums) {
-        int max = 0;
-        Set<Integer> set = IntStream.of(nums).boxed().collect(Collectors.toSet());
-
-        for (int i = 0; i < nums.length; i++) {
+        for (int num: nums) {
             int consecutive = 1;
-            int target = nums[i];
+            int target = num;
             while(set.contains(--target)) {
                 consecutive++;
                 set.remove(target);
             }
-            target = nums[i];
+            target = num;
             while(set.contains(++target)) {
                 consecutive++;
                 set.remove(target);
