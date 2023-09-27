5838cb984423a9a990ce5cd115e49cfe7701da93
watchmen-r <watchmen0205@yahoo.co.jp>
2023-09-28 07:06:01 +0900

    review two sum 2

--- MediumProblems/TwoSum2.java
+++ MediumProblems/TwoSum2.java
@@ -1,21 +1,21 @@
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
-        int left = 0;
-        int right = numbers.length - 1;
+        int begin = 0;
+        int end = numbers.length - 1;
 
-        while (left < right) {
-            int sum = numbers[left] + numbers[right];
-
+        while (begin < end) {
+            int sum = numbers[begin] + numbers[end];
             if (sum == target) {
                 break;
             }
 
             if (sum < target) {
-                left++;
+                begin++;
             } else {
-                right--;
+                end--;
             }
         }
-        return new int[] { left + 1, right + 1 };
+
+        return new int[]{begin + 1, end + 1};
     }
 }
