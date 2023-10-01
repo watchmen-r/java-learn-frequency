0424c5921c59abcb90955fa0fec9307c49f1cae5
watchmen-r <watchmen0205@yahoo.co.jp>
2023-10-01 15:52:34 +0900

    review product array except

--- MediumProblems/3Sum.java
+++ MediumProblems/3Sum.java
@@ -1,33 +1,38 @@
-class 3Sum {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
-        List<List<Integer>> res = new ArrayList<>();
+        List<List<Integer>> ans = new ArrayList<>();
 
-        for(int i = 0; i < nums.length && nums[i] <= 0; i++) {
-            if(i == 0 || nums[i-1] != nums[i]) {
-                twoSum(nums, i, res);
-            }
-        }
-        return res;
-    }
+        for (int i = 0; i < nums.length && nums[i] <= 0; i++) {
+            if (i == 0 || nums[i-1] != nums[i]) {
+                int target = 0 - nums[i];
+                int left = i + 1;
+                int right = nums.length - 1;
 
-    static void twoSum(int[] nums, int i, List<List<Integer>> res) {
-        int lo = i + 1, hi = nums.length - 1;
-        while(lo < hi) {
-            int sum = nums[i] + nums[lo] + nums[hi];
-            // sortしているのでマイナスであればloを＋に移動させる
-            if(sum < 0) {
-                lo++;
-            } else if(sum > 0) {
-                hi--;
-            } else {
-                res.add(Arrays.asList(nums[i], nums[lo], nums[hi]));
-                lo++; hi--;
-                // 次のloやhiが同じだった場合は、同じ答えになってしまうため飛ばす
-                while(lo < hi && nums[lo] == nums[lo - 1]) {
-                    lo++;
+                while (left < right) {
+                    if (nums[left] + nums[right] == target) {
+                        ans.add(Arrays.asList(nums[i], nums[left], nums[right]));
+
+                        while(left < right && nums[left] == nums[left + 1]) {
+                            left++;
+                        }
+                        while(left < right && nums[right] == nums[right - 1]) {
+                            right--;
+                        }
+                        left++;
+                        right--;
+                    } else if (nums[left] + nums[right] > target) {
+                        right--;
+                    } else {
+                        left++;
+                    }
                 }
             }
         }
+        return ans;
     }
 }
--- MediumProblems/ProductArrayExcept.java
+++ MediumProblems/ProductArrayExcept.java
@@ -1,16 +1,18 @@
-class ProductArrayExcept {
+class Solution {
     public int[] productExceptSelf(int[] nums) {
         int[] answer = new int[nums.length];
+
         answer[0] = 1;
         for (int i = 1; i < nums.length; i++) {
-            answer[i] = answer[i-1] * nums[i-1];
+            answer[i] = nums[i - 1] * answer[i - 1];
         }
 
         int right = 1;
         for (int i = nums.length - 1; i >= 0; i--) {
-            answer[i] = right * answer[i];
+            answer[i] = answer[i] * right;
             right *= nums[i];
         }
+
         return answer;
     }
 }
